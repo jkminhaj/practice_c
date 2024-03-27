7ae6138a5f365b766c6096a063b9217ecdc973ac
Minhajul Islam <mmminhaj221@gmail.com>
2024-03-27 06:46:19 +0600

    sloved problem 2

--- practice.c
+++ practice.c
@@ -14,3 +14,68 @@ int main () {
 
     return 0 ;
 }
+
+// 2. Write a C program to print a block F using the hash (#), where the F has a height of six characters and width of five and four characters. And also print a very large 'C'.
+// Expected Output:
+
+// ######
+// #
+// #
+// #####
+// #
+// #
+// #
+//     ######
+//   ##      ##
+//  #
+//  #
+//  #
+//  #
+//  #
+//   ##      ##
+//     ######
+
+#include <stdio.h>
+int main () {
+    
+    // This is F
+    printf("#####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("####\n");
+    printf("#\n");
+    printf("#\n");
+
+    // This is C
+    printf("             #############################\n");
+    printf("             #############################\n");
+    printf("   ##########                           ##########\n");
+    printf("   ##########                           ##########\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("###\n");
+    printf("   ##########                           ##########\n");
+    printf("   ##########                           ##########\n");
+     printf("             #############################\n");
+    printf("             #############################\n");
+    
+    
+
+    return 0 ;
+}
